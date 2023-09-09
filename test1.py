16ae564c521ce83e88ee9e3ad7e354265cc38c3a
Firstivan <geraschenko.iv@mail.ru>
2023-09-09 21:31:29 +0300

    Добавил условия 7 задачи, без решения пятой

--- test1.py
+++ test1.py
@@ -241,13 +241,13 @@ print(1)'''
 # ______________________
 
 
-# line = ""
-# for i in range(5):
-#     line = ""
-#     for j in range(5):
-#         line += "*"
-#     print(line)
-# 
+line = ""
+for i in range(5):
+    line = ""
+    for j in range(5):
+        line += "*"
+    print(line)
+
 
 
 # text = 'СъЕШЬ ещё этих МяГкИх французских булок'
@@ -259,16 +259,16 @@ print(1)'''
 # _______________________
 
 
-text = 'съешь ещё этих мягких французских булок'
-print(text[0]) # c
-print(text[1]) # ъ
-print(text[len(text)-1]) # к
-print(text[-5]) # б
-print(text[:]) # съешь ещё этих мягких французских булок
-print(text[:2]) # съ
-print(text[len(text)-2:]) # ок
-print(text[2:9]) # ешь ещё
-print(text[6:-18]) # ещё этих мягких
-print(text[0:len(text):6]) # сеикакл
-print(text[::6]) # сеикакл
-text = text[2:9] + text[-5] + text[:2] # ...
+# text = 'съешь ещё этих мягких французских булок'
+# print(text[0]) # c
+# print(text[1]) # ъ
+# print(text[len(text)-1]) # к
+# print(text[-5]) # б - считает с конца
+# print(text[:]) # съешь ещё этих мягких французских булок
+# print(text[:2]) # съ
+# print(text[len(text)-2:]) # ок
+# print(text[2:9]) # ешь ещё
+# print(text[6:-18]) # ещё этих мягких
+# print(text[0:len(text):6]) # сеикакл
+# print(text[::6]) # сеикакл
+# text = text[2:9] + text[-5] + text[:2] # ...
