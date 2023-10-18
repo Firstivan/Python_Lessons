e40f835df182765fb088441c6aa8baedf3a2d5da
Firstivan <geraschenko.iv@mail.ru>
2023-10-18 22:28:45 +0300

    Удадил лишнее из решения задачи 44

--- HWPython_44.py
+++ HWPython_44.py
@@ -8,43 +8,18 @@
 # lst += ['human'] * 10
 # random.shuffle(lst)
 # data = pd.DataFrame({'whoAmI':lst})
-# # data.head()
-
-# one_hot_data = pd.get_dummies(data['whoAmI'])
-# data = pd.concat([data, one_hot_data], axis=1)
 # data.head()
-# _____________________________________
-
-
-
-# import random
  
-# lst = ['robot'] * 10
-# lst += ['human'] * 10
-# random.shuffle(lst)
-# data = pd.DataFrame({'whoAmI': lst})
-# print(data)
- 
-# # Добавляем временный столбец 'tmp' со значениями 1
-# data['tmp'] = 1
-# # Устанавливаем индекс по столбцам 'whoAmI' и 'tmp'
-# data.set_index([data.index, 'whoAmI'], inplace=True)
-# # Делаем распаковку столбца 'whoAmI' в новые столбцы
-# data = data.unstack(level=-1, fill_value=0).astype(int)
-# data.columns = data.columns.droplevel()
-# data.columns.name = None
-# print(data)
-# __________________________________
 
 
 import pandas as pd
-
 import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
-
-one_hot = data['whoAmI'].str.get_dummies()
-
-one_hot.head()
+categories = data['whoAmI'].unique()
+category_dict = {category: [1 if cat == category else 0 for cat in categories] for category in categories}
+one_hot_data = pd.DataFrame(data['whoAmI'].apply(lambda x: category_dict[x]).tolist(), columns=categories)
+data = pd.concat([data, one_hot_data], axis=1)
+data.head()
