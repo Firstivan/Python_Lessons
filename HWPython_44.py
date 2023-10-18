5ce6d28470649a7e8493eb1dcdf6d61dda9e131f
Firstivan <geraschenko.iv@mail.ru>
2023-10-18 21:59:26 +0300

    Добавил последнюю задачу №44

---
+++ HWPython_44.py
@@ -0,0 +1,18 @@
+# Задача 44:
+# В ячейке ниже представлен код генерирующий DataFrame, 
+# которая состоит всего из 1 столбца. Ваша задача перевести его 
+# в one hot вид. Сможете ли вы это сделать без get_dummies?
+
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+# data.head()
+
+one_hot_data = pd.get_dummies(data['whoAmI'])
+
+# Объединяем one hot данные с исходным DataFrame
+data = pd.concat([data, one_hot_data], axis=1)
+
+data.head()
